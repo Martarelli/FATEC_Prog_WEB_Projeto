8d74182b322bc96c9cd8e60e85aa652d523bc36e
Martarelli <renanmartarelli@gmail.com>
2023-12-01 14:16:28 -0300

    imagens e exibição dados db

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\DAO\PizzaDAO;
+use App\Models\DAO\ClienteDAO;
+use App\Models\DAO\UsuarioDAO;
+
 class HomeController extends Controller
 {
     public function index()
     {
         if (!$this->auth()) $this->redirect('/login/index');
-        
+        $ClienteDAO = new ClienteDAO();
+        $PizzaDAO = new PizzaDAO();
+        $UsuarioDAO = new UsuarioDAO();
+
+        self::setViewParam('listaCliente', $ClienteDAO->listar());
+        self::setViewParam('listaPizza', $PizzaDAO->listar());
+        self::setViewParam('listaUsuarios', $UsuarioDAO->listar());
+
         $this->render('home/index');
     }
 }
--- App/Views/home/index.php
+++ App/Views/home/index.php
@@ -1,7 +1,44 @@
-<div class="container w-100 m-0 p-0">
+<div class="w-100 m-0 p-0">
 
-    <div class="container m-0 p-0 bg-success" style="min-height: 25vh; min-width: 100vw; ">
-        <img src="public/images/banner-min.jpg" alt="banner" style="min-height: 25vh; max-width: 100vw; "class="img-fluid">
+    <div class="w-100 m-0 p-0" style="width: 100% !important;">
+        <img src="public/images/banner-min.jpg" alt="banner"  class="img-fluid">
     </div>
+
+
+    <div class="w-100 d-flex justify-content-center align-items-center p-4 gap-5">
+        <div class="card text-center" style="width: 18rem;">
+            <img src="public/images/user-img.png" class="card-img-top p-3" alt="user img">
+            <div class="card-body">
+            <h5 class="card-title">Usuários Cadastrados</h5>
+            <p class="card-text"><?=count($viewVar['listaUsuarios']);?></p>
+            <a href="http://<?php echo APP_HOST . "/usuario"; ?>" class="btn btn-dark">Listagem Usuários</a>
+            </div>
+        </div>
+        
+        <div class="card text-center" style="width: 18rem;">
+            <img src="public/images/pizza-img.png" class="card-img-top" alt="user img">
+            <div class="card-body">
+            <h5 class="card-title">Pizzas Cadastrados</h5>
+            <p class="card-text"><?=count($viewVar['listaPizza']);?></p>
+            <a href="http://<?php echo APP_HOST . "/pizza"; ?>" class="btn btn-dark">Listagem Pizzas</a>
+            </div>
+        </div>
+        
+        <div class="card text-center" style="width: 18rem;">
+            <img src="public/images/cliente-img.png" class="card-img-top p-3" alt="user img">
+            <div class="card-body">
+            <h5 class="card-title">Clientes Cadastrados</h5>
+            <p class="card-text"><?=count($viewVar['listaCliente']);?></p>
+            <a href="http://<?php echo APP_HOST . "/cliente"; ?>" class="btn btn-dark">Listagem Clientes</a>
+            </div>
+        </div>
+
+    </div>
+    
+
+
+
+
+
 
 </div>
